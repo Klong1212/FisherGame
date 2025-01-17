45b75e8d0e3c974bc027cd2d424dc2c535f609f2
Tanada Nawakata <klong8257@gmail.com>
2025-01-17 19:02:28 +0700

    ทดลองเพิ่มการขยับของวัตถุ

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 from kivy.app import App
 from kivy.uix.floatlayout import FloatLayout
-from kivy.properties import StringProperty
-
+from kivy.properties import NumericProperty
+from kivy.clock import Clock
+import time
 class FishGame(FloatLayout):
-    score = StringProperty("0")
+    hi = NumericProperty(0)
 
     def __init__(self, **kwargs):
         super(FishGame, self).__init__(**kwargs)
+        Clock.schedule_interval(self.update,0.1)
 
-    def on_button_click(self, instance):
+    def update(self, dt):
+        if self.hi > 0:
+            self.hi -= 1
+    def on_button_click(self):
         print(f"Button clicked!")
-        self.score = str(int(self.score) + 1)
+        self.hi += 10
 
 class FishApp(App):
     def build(self):  
