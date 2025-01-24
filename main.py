58de371c758f891637332706b7c8810fe3558466
Tanada Nawakata <klong8257@gmail.com>
2025-01-24 21:46:08 +0700

    fix speed add widget fish

--- main.py
+++ main.py
@@ -8,9 +8,10 @@ from kivy.uix.widget import Widget
 
 class BagScreen(Screen):
     def __init__(self, **kwargs):
-        super(Screen, self).__init__(**kwargs)
+        super(BagScreen, self).__init__(**kwargs)
         self.back = False
-
+class Fish(Screen):
+    pass
 class MainScreen(Screen):
     hi = NumericProperty(0)
     hi_game = NumericProperty(0)
@@ -23,32 +24,30 @@ class MainScreen(Screen):
         Clock.schedule_interval(self.minigame, 1.0/60.0)
         self.ispress = False
         self.minigame_up = False
-        self.rand_fish = [1,2,3,4,5,6,7,8,9,10]
+        self.rand_fish = [n for n in range(1,101)]
         self.video = False
         self.bag = False
     def minigame(self, dt):
         if self.start:
             self.rand=randint(1,10)
             self.rand_fight=randint(1,50)
-            self.fish = choice(self.rand_fish)
-            if self.fish in [1,2,3,4,5]:
-                self.fish = 5
+
             if self.hi > 0:
                 self.hi -= 5
             if self.ispress and self.hi < 480:
                 self.hi += 10
             if self.rand_fight==1:
-                self.speed=randint(1,self.fish)
+                self.speed=randint(self.speed_fish//2,self.speed_fish)
             if self.hi_game<=0:
                 self.minigame_up = True
-                self.speed=randint(1,self.fish)
+                self.speed=randint(self.speed_fish//2,self.speed_fish)
             if self.hi_game>=480:
                 self.minigame_up = False
-                self.speed=randint(1,self.fish)
+                self.speed=randint(self.speed_fish//2,self.speed_fish)
             
             if self.hi>self.hi_game and self.hi< self.hi_game+50 and self.rand==1:
-                self.minigame_up=~self.minigame_up
-                self.speed=randint(1,self.fish)
+                self.minigame_up=not(self.minigame_up)
+                self.speed=randint(self.speed_fish//2,self.speed_fish)
         
             if self.minigame_up:
                 self.hi_game += self.speed
@@ -56,14 +55,14 @@ class MainScreen(Screen):
                 self.hi_game -= self.speed
             if self.hi>self.hi_game and self.hi< self.hi_game+50:
                 self.score +=5
-            if ~(self.hi>self.hi_game and self.hi< self.hi_game+50) and self.score > 0:
+            if not(self.hi>self.hi_game and self.hi< self.hi_game+50) and self.score > 0:
                 self.score -=1
             if self.score >= 500:
                 self.start = False
                 self.score = 0
                 self.hi = 0
                 self.hi_game = 0
-                self.link = 'fishpic/animation1.gif'
+                self.link = 'animation1.gif'
             if self.video:
                 self.link = "animation2.gif"
                 self.video = False
@@ -74,6 +73,15 @@ class MainScreen(Screen):
     def on_button_up(self):
         self.ispress =False
     def on_click(self):
+        self.fish = choice(self.rand_fish)
+        if self.fish in [i for i in range(1,51)]:
+            self.speed_fish = 4
+        elif self.fish in [i for i in range(51,71)]:
+            self.speed_fish = 6
+        elif self.fish in [i for i in range(71,96)]:
+            self.speed_fish = 8
+        elif self.fish in [i for i in range(96,101)]:
+            self.speed_fish = 10
         self.start = True
         self.video=True
 
