0e36ab5ca655d9b403b2f66f171017f2ee3e994a
Tanada Nawakata <klong8257@gmail.com>
2025-01-24 22:25:42 +0700

    เพิ่มฟังชั่นใส่ภาพ

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ class MainScreen(Screen):
     score = NumericProperty(0)
     fish_come = BooleanProperty(False)
     link = StringProperty('animation1.gif')
+    fish_link= StringProperty()
     def __init__(self, **kwargs):
         super(MainScreen, self).__init__(**kwargs)
         Clock.schedule_interval(self.minigame, 1.0/60.0)
@@ -27,6 +28,15 @@ class MainScreen(Screen):
         self.rand_fish = [n for n in range(1,101)]
         self.video = False
         self.bag = False
+        self.fish_com=['fishpic/Sprite-0004.jpg','fishpic/Sprite-0005.jpg','fishpic/Sprite-0006.jpg','fishpic/Sprite-0007.jpg',
+                       'fishpic/Sprite-0008.jpg','fishpic/Sprite-0009.jpg','fishpic/Sprite-00010.jpg','fishpic/Sprite-00011.jpg',
+                       'fishpic/Sprite-00012.jpg','fishpic/Sprite-00013.jpg','fishpic/Sprite-0014.jpg','fishpic/Sprite-0015.jpg',
+                       ]
+        self.fish_rare=['fishpic/Sprite-0019.jpg','fishpic/Sprite-0020.jpg','fishpic/Sprite-0021.jpg','fishpic/Sprite-0022.jpg',
+                        'fishpic/Sprite-0023.jpg','fishpic/Sprite-0024.jpg','fishpic/Sprite-0025.jpg']
+        self.fish_epic=['fishpic/Sprite-0016.jpg','fishpic/Sprite-0017.jpg','fishpic/Sprite-0018.jpg','fishpic/Sprite-0026.jpg',
+                        'fishpic/Sprite-0027.jpg','fishpic/Sprite-0028.jpg','fishpic/Sprite-0029.jpg','fishpic/Sprite-0030.jpg']
+        self.fish_legendary=['fishpic/Sprite-0031.jpg','fishpic/Sprite-0032.jpg','fishpic/Sprite-0033.jpg']
     def minigame(self, dt):
         if self.start:
             self.rand=randint(1,10)
@@ -67,7 +77,9 @@ class MainScreen(Screen):
                 self.link = "animation2.gif"
                 self.video = False
                 
-        
+    def update_button_image(self, button_id, speed_fish):
+        button = self.ids[button_id]
+        button.background_normal = speed_fish
     def on_button_down(self):
         self.ispress = True
     def on_button_up(self):
@@ -76,12 +88,16 @@ class MainScreen(Screen):
         self.fish = choice(self.rand_fish)
         if self.fish in [i for i in range(1,51)]:
             self.speed_fish = 4
+            self.fish_link=choice(self.fish_com)
         elif self.fish in [i for i in range(51,71)]:
             self.speed_fish = 6
+            self.fish_link=choice(self.fish_rare)
         elif self.fish in [i for i in range(71,96)]:
             self.speed_fish = 8
+            self.fish_link=choice(self.fish_epic)
         elif self.fish in [i for i in range(96,101)]:
             self.speed_fish = 10
+            self.fish_link=choice(self.fish_legendary)
         self.start = True
         self.video=True
 
